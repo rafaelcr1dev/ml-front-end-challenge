eea515173a3fd1099725ee81d7e67efd84a93d38
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-11-20 20:41:37 -0300

    feat(home): add the main metatags on head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,26 @@ const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title>Mercado libre | Mejores tratos</title>
+        <link
+          href="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.17.0/mercadolibre/favicon.svg"
+          rel="icon"
+          data-head-react="true"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="La comunidad de compra y venta online más grande de América Latina."
+        />
+        <meta
+          property="og:image"
+          content="https://http2.mlstatic.com/static/org-img/homesnw/mercado-libre.png"
+        />
       </Head>
 
       <main>
