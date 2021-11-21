cacb4df104a481f499eb92b0490df7731cc487bc
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-11-21 00:34:33 -0300

    refactor(header): change text of H1 on HomeComponent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,7 @@ import Container from '@/components/Container'
 const Home: React.FC<AppProps> = () => {
   return (
     <Container>
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <h1>Mercado libre</h1>
     </Container>
   )
 }
