3d3a5378b1b48aba860b3c2b6f33108a73a6c341
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-11-21 00:19:40 -0300

    feat(settings): add collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   verbose: true,
+  roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
@@ -7,4 +8,8 @@ module.exports = {
     '\\.(css|less|scss)$': '<rootDir>jest.cssTransform.js',
   },
   testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.tsx',
+    '!<rootDir>/src/components/**/index.tsx',
+  ],
 };
