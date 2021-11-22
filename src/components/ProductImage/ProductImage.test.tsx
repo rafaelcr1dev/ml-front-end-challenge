bb38b082e74fb0cad526f1477228199f9e61336e
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-11-21 23:07:37 -0300

    test(search-result): add ProductImage

--- src/components/ProductImage/ProductImage.test.tsx
+++ src/components/ProductImage/ProductImage.test.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { render } from '@testing-library/react'
+
+import ProductImage from '@/components/ProductImage'
 
 describe('ProductImage Component', () => {
-  it('should sum 2 + 2 equal 4', () => {
-    expect(2 + 2).toEqual(4)
+  it('should display only one image', () => {
+    const { container } = render(
+      <ProductImage url="mercadolibre.png" title="Mercado Libre" />
+    )
+
+    const image = container.querySelector('img')
+    const images = container.querySelectorAll('img')
+
+    expect(images).toHaveLength(1)
+
+    expect(image).toHaveAttribute('itemprop', 'image')
+    expect(image).toHaveAttribute('src', 'mercadolibre.png')
+    expect(image).toHaveAttribute('alt', 'Mercado Libre')
   })
 })
